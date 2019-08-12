86fb63785836abe9831fc58c37d6ee2d88d0817c
robertistok <robertistok94@gmail.com>
2019-08-12 18:59:53 +0200

    off we go

---
+++ Procfile
@@ -0,0 +1,2 @@
+iPhoneXS-64-like-new: node index.js --filter_condition=nyskick --filter_memory=64gb
+iPhoneXS: node index.js
---
+++ index.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+
+const axios = require("axios");
+
+const SWAPPIE_URL = "https://swappie.com/se/modell/iphone-xs";
+const FILTER_PARAMS = ["filter_condition", "filter_memory"];
+
+const HEALTHCHECKS_URL = process.env.HEALTHCHECKS_URL;
+
+const run = async () => {
+  const params = process.argv.reduce((acc, arg) => {
+    if (FILTER_PARAMS.some(p => arg.includes(p))) {
+      const [key, value] = arg.split("=");
+      return { ...acc, [key.replace("--", "")]: value };
+    }
+
+    return acc;
+  }, {});
+
+  const { data: html } = await axios({
+    url: SWAPPIE_URL,
+    params
+  });
+
+  const isAvailable = !html.includes("Tyvärr är mobilen du söker");
+
+  if (isAvailable) {
+    console.log("Your desired phone is available! Go and get it now.", params);
+    await axios({ url: HEALTHCHECKS_URL });
+  } else {
+    console.log("Not yet, keep trying!", params);
+    await axios({ url: `${HEALTHCHECKS_URL}/fail` });
+  }
+
+  process.exit();
+};
+
+run();
